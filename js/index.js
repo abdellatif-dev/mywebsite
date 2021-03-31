22f0804f23f80fa6544e7aa9b39c09998c995826
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-03-31 12:11:30 +0000

    master

--- js/index.js
+++ js/index.js
@@ -1,31 +1,36 @@
+// style element
 const css = document.getElementById("css")
 
-let is_dark = localStorage.getItem('is_dark');
-savecookie();
+//  load from  localStorage 
+let is_dark = localStorage.getItem('is_dark') || true;
 
-if (is_dark == "true") {
+save_localStorage();
+
+// toggle light/dark theme before loading the page
+if (is_dark != "false") {
     css.href = "css/style.css";
 
 } else {
     css.href = "css/light_style.css";
-
 }
 
+// toggle light/dark theme onclick()
 function light_dark() {
     if (is_dark == "true") {
         css.href = "css/light_style.css";
         is_dark = "false";
-        savecookie()
+        save_localStorage()
 
     } else {
         css.href = "css/style.css";
         is_dark = "true";        
-        savecookie()
+        save_localStorage()
 
     }
 
 }
 
-function savecookie() {
+// save to localStorage
+function save_localStorage() {
     localStorage.setItem('is_dark', is_dark);
 }
