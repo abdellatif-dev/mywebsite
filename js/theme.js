0f42854802c3a36e0fb3991cb1303bb24ff31b06
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-05-26 01:52:17 +0100

    404

--- js/theme.js
+++ js/theme.js
@@ -8,51 +8,52 @@ let is_dark = localStorage.getItem('is_dark');
 theme_onload();
 
 function theme_onload() {
-    if (is_dark != "light") {
-        css.href = "css/style.css";
+  if (is_dark != "light") {
+    css.href = "css/style.css";
 
-    } else {
-        css.href = "css/light_style.css";
+  } else {
+    css.href = "css/light_style.css";
 
-    }
+  }
 
-    themebtn_txt();
-    save_localStorage();
+  themebtn_txt();
+  save_localStorage();
 
 }
 
 function themebtn_txt() {
-    if (is_dark == "light") {
-        theme_btn.textContent = "dark theme";
+  if (is_dark == "light") {
+    theme_btn.textContent = "dark theme";
 
-    } else {
-        theme_btn.textContent = "light theme";
+  } else {
+    theme_btn.textContent = "light theme";
 
-    }
+  }
 }
 
 theme_btn.addEventListener('click', light_dark);
 
 // toggle light/dark theme onclick()
 function light_dark(e) {
-    e.preventDefault();
-    if (is_dark == "dark") {
-        css.href = "css/light_style.css";
-        is_dark = "light";
-        themebtn_txt();
-        save_localStorage()
+  e.preventDefault();
+  if (is_dark == "dark") {
+    css.href = "css/light_style.css";
+    is_dark = "light";
+    themebtn_txt();
+    save_localStorage()
 
-    } else {
-        css.href = "css/style.css";
-        is_dark = "dark";
-        themebtn_txt();
-        save_localStorage()
+  } else {
+    css.href = "css/style.css";
+    is_dark = "dark";
+    themebtn_txt();
+    save_localStorage()
 
-    }
+  }
 
 }
 
 // save to localStorage
 function save_localStorage() {
-    localStorage.setItem('is_dark', is_dark);
+  localStorage.setItem('is_dark', is_dark);
 }
+
