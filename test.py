60624c93e8da1d3f574c6696734788912753f9d0
abdellatif-dev <abdellatif.devlog@gmail.com>
2021-07-09 15:17:37 +0100

    merge

--- test.py
+++ test.py
@@ -7,15 +7,19 @@ PORT = 5500
 HOST = "127.0.0.1"
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
-    try:
-        print(f"serving at {HOST}:{PORT}")
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        httpd.shutdown()
-        exit("\nclosing dev server...")
-    except :
-        httpd.shutdown()
-        exit("\nerror happened...")
+try:
+    with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+        try:
+            print(f"serving at {HOST}:{PORT}")
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            httpd.shutdown()
+            exit("\nclosing dev server...")
+        except :
+            httpd.shutdown()
+            exit("\nerror happened...")
 
-httpd.shutdown()
+    httpd.shutdown()
+except Exception as e:
+    print(e)
+
